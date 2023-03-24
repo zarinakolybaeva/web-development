9e5a3635809675797432eef5d428376aef51ce78
Zarina <kolybayevazarina04@gmail.com>
2023-03-25 04:07:31 +0600

    lab 7

--- lab 7/task2/test/Scripts/activate.bat
+++ lab 7/task2/test/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\админ\Web-Dev\lab 7\task2\test
+set VIRTUAL_ENV=C:\Users\админ\Web-Dev\labb7\test
 
 if not defined PROMPT set PROMPT=$P$G
 
